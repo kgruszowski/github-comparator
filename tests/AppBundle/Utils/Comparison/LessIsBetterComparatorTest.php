3c1aedfcc49c6d275f1834661aa651b767ddb592
Kamil Gruszowski <kamil.gruszowski@gmail.com>
2018-09-05 10:59:27 +0200

    refactoring tests

--- tests/AppBundle/Utils/Comparison/LessIsBetterComparatorTest.php
+++ tests/AppBundle/Utils/Comparison/LessIsBetterComparatorTest.php
@@ -14,34 +14,37 @@ class LessIsBetterComparatorTest extends TestCase
      */
     public function testInvalidArgument()
     {
-        $issueComparator = new LessIsBetterComparator();
+        $issueComparator = new LessIsBetterComparator('issues');
         $issueComparator->compare('dummy', 2);
     }
 
     public function testValueAWinner()
     {
-        $issueComparator = new LessIsBetterComparator();
+        $issueComparator = new LessIsBetterComparator('issues');
         $metric = $issueComparator->compare(10, 2);
 
         $this->assertInstanceOf(Metric::class, $metric);
+        $this->assertEquals('issues', $metric->getMetricName());
         $this->assertEquals(1, $metric->getWinner());
     }
 
     public function testValueBWinner()
     {
-        $issueComparator = new LessIsBetterComparator();
+        $issueComparator = new LessIsBetterComparator('issues');
         $metric = $issueComparator->compare(10, 20);
 
         $this->assertInstanceOf(Metric::class, $metric);
+        $this->assertEquals('issues', $metric->getMetricName());
         $this->assertEquals(-1, $metric->getWinner());
     }
 
     public function testDraw()
     {
-        $issueComparator = new LessIsBetterComparator();
+        $issueComparator = new LessIsBetterComparator('issues');
         $metric = $issueComparator->compare(10, 10);
 
         $this->assertInstanceOf(Metric::class, $metric);
+        $this->assertEquals('issues', $metric->getMetricName());
         $this->assertEquals(0, $metric->getWinner());
     }
 
--- tests/AppBundle/Utils/Comparison/MoreIsBetterComparatorTest.php
+++ tests/AppBundle/Utils/Comparison/MoreIsBetterComparatorTest.php
@@ -14,34 +14,37 @@ class MoreIsBetterComparatorTest extends TestCase
      */
     public function testInvalidArgument()
     {
-        $starComparator = new MoreIsBetterComparator();
+        $starComparator = new MoreIsBetterComparator('stars');
         $starComparator->compare('dummy', 2);
     }
 
     public function testValueAWinner()
     {
-        $starComparator = new MoreIsBetterComparator();
+        $starComparator = new MoreIsBetterComparator('stars');
         $metric = $starComparator->compare(10, 2);
 
         $this->assertInstanceOf(Metric::class, $metric);
+        $this->assertEquals('stars', $metric->getMetricName());
         $this->assertEquals(-1, $metric->getWinner());
     }
 
     public function testValueBWinner()
     {
-        $starComparator = new MoreIsBetterComparator();
+        $starComparator = new MoreIsBetterComparator('stars');
         $metric = $starComparator->compare(10, 20);
 
         $this->assertInstanceOf(Metric::class, $metric);
+        $this->assertEquals('stars', $metric->getMetricName());
         $this->assertEquals(1, $metric->getWinner());
     }
 
     public function testDraw()
     {
-        $starComparator = new MoreIsBetterComparator();
+        $starComparator = new MoreIsBetterComparator('stars');
         $metric = $starComparator->compare(10, 10);
 
         $this->assertInstanceOf(Metric::class, $metric);
+        $this->assertEquals('stars', $metric->getMetricName());
         $this->assertEquals(0, $metric->getWinner());
     }
 
